131e515d392a23c1229e1762f04d08fd2a841b35
Sagar <sagarsurya45@gmail.com>
2025-04-17 19:00:18 +0530

    frontend and tailwind

---
+++ Web Development/e-commerce/buyandtry/client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+   <>
+   <h1 className='text-red-400 bg-yellow-500'>starts with frontend</h1>
+   </>
+  )
+}
+
+export default App
